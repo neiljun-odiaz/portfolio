d1d3c24e805b56911093e20ced80f14cef1f47df
Neiljun I. Odiaz <neiljun.odiaz@gmail.com>
2016-11-01 10:10:27 +0800

    Initial commit

---
+++ js/components/skill.vue
@@ -0,0 +1,10 @@
+<template id="child-template">
+	<span>{{ message }}</span>
+</template>
+
+<script>
+	module.exporst = {
+		props: ['message'],
+  		template: '#child-template'
+	}
+</script>
---
+++ js/script.js
@@ -0,0 +1,10 @@
+// const Child = require('./components/skill.vue')
+
+// Vue.component('child', Child)
+
+// new Vue ({
+// 	el: '#portfolio',
+// 	data: {
+// 		myname: 'Neiljun I. Odiaz'
+// 	}
+// })
