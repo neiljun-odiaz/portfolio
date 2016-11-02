7825d7b88a6795c9da0a3573aaa52f7b5dd96e68
Neiljun I. Odiaz <neiljun.odiaz@gmail.com>
2016-11-02 21:59:36 +0800

    Change ; Minor layout tweaks

--- js/script.js
+++ js/script.js
@@ -8,3 +8,21 @@
 // 		myname: 'Neiljun I. Odiaz'
 // 	}
 // })
+
+jQuery('document').ready(function(){
+    $(document).on( 'scroll', function(){
+        var scroll = $(document).scrollTop();
+
+        if ( scroll > 30 ) {
+            $('.Body__progressbg').css('top', '0px' );
+        } else {
+            $('.Body__progressbg').css('top', '30px' );
+        }
+        
+        if ( scroll > 30 ) {
+            $('.Body__progressbg').css('height', '450px');
+        } else {
+            $('.Body__progressbg').css('height', '246px');
+        }
+    });
+});
