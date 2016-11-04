ce27fb7f527f66a8c6207efaeae0524f58580e45
Neiljun I. Odiaz <neiljun.odiaz@gmail.com>
2016-11-04 16:39:29 +0800

    Update mobile layout nav menu;

--- js/script.js
+++ js/script.js
@@ -11,56 +11,79 @@
 
 jQuery('document').ready(function(){
     var lastScrollTop = 0;
-    var new_top = 0;
-    var new_height = 0;
+    var new_top = 60;
+    var new_height = 228;
+
+    timeline_progress();
+
     $(document).on( 'scroll', function(){
+        timeline_progress();
+    });
+
+    // function get timeline progress by scroll top
+    function timeline_progress() {
         var scroll = $(document).scrollTop();
         var bg_top = parseFloat($('.Body__progressbg').css('top'));
         var bg_height = parseFloat($('.Body__progressbg').css('height'));
-        // if ( scroll > 50 ) {
-        //     $('.Body__progressbg').css('top', '0px' );
-        // } else {
-        //     $('.Body__progressbg').css('top', '50px' );
-        // }
-        
-        // if ( scroll > 50 ) {
-        //     $('.Body__progressbg').css('height', '450px');
-        // } else {
-        //     $('.Body__progressbg').css('height', '246px');
-        // }
-        // Check if scrolled down or up
-        if ( lastScrollTop < scroll ) { // Scroll down
-            if ( bg_top > 0 )
-                new_top = parseFloat(bg_top) - parseFloat(1.3);
 
-            if ( bg_height < 380 )
-                new_height = parseFloat(bg_height) + parseFloat(1.2);
-        } else { // Scroll up
-            if ( scroll < 350 )
-                new_top = parseFloat(bg_top) + parseFloat(1.8);
-
-            if ( bg_height > 246 )
-                new_height = parseFloat(bg_height) - parseFloat(1.5);
+        if ( lastScrollTop == 0 && scroll > 60 ) {
+            console.log(scroll);
+            bg_top = 0;
+            new_top = 0;
         }
 
-        // console.log( lastScrollTop - scroll );
-        // console.log(scroll);
+        // Check if scrolled down or up
+        if ( lastScrollTop < scroll ) { // Scroll down
+            if ( scroll > 60 ) {
+                if ( bg_top > 0 )
+                    new_top = parseFloat(bg_top) - parseFloat(5);
+
+                if ( bg_height < 328 )
+                    new_height = parseFloat(bg_height) + parseFloat(1.2);
+            }
+        } else { // Scroll up
+            if ( scroll < 70  && bg_top < 70 )
+                new_top = parseFloat(bg_top) + parseFloat(15);
+
+            if ( scroll < 184 )
+                new_height = parseFloat(bg_height) - parseFloat(1.3);
+
+            console.log(bg_top);
+        }
 
         $('.Body__progressbg').css('top',new_top+'px');
         $('.Body__progressbg').css('height',new_height+'px');
-
         lastScrollTop = scroll;
-    });
+    }
 
+
+    // Show menu
     $('.toggle_menu .btn').on('click', function(e){
         e.preventDefault();
-        $('.Header').toggleClass('active');
-        $('.Body__content').toggleClass('moveleft');
+        toggle_menu();
     });
 
+    // Hide Menu
     $('.close_btn').on('click', function(e){
         e.preventDefault();
-        $('.Header').removeClass('active');
-        $('.Body__content').removeClass('moveleft');
+        toggle_menu();
     });
+
+    // Toggle Menu
+    function toggle_menu() {
+        $('.Header').toggleClass('active');
+        $('.Body__content').toggleClass('moveleft');
+    }
+
+    // Scroll to section when an item in menu is clicked
+    $('.Header__navlink').on('click', function(e){
+        e.preventDefault();
+        var this_ = $(this);
+        var target = $( '#' + this_.data('section') );
+        console.log(target);
+        $('html, body').animate({
+            scrollTop: target.offset().top
+        }, 500);
+    });
+
 });
